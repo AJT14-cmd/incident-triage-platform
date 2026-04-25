dbaf4c7ba66fd8de10f262001957d2fb0064e0af
Anuj Jung Thapa <anujcaeli@gmail.com>
2026-04-24 19:39:11 -0700

    Successfully able to run with flyway now

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.flywaydb.core.Flyway;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Testcontainers
 class DemoApplicationTests {
+    
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired Flyway flyway;
+    @Test
+    void shouldConnectToDatabase() {
+        assertTrue(flyway.info().pending().length == 0);
+    }
 
 }
